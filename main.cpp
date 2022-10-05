422a50934623dafc26426e5b53efa7053e25ba4e
Glowman554 <glowman434@gmail.com>
2022-10-05 19:23:54 +0200

    blink example

--- main.cpp
+++ main.cpp
@@ -5,55 +5,13 @@
 
 #include "mbed.h"
 
-int segments[] = {
-    0b00111111,
-    0b00000110,
-    0b01011011,
-    0b01001111,
-    0b01100110,
-    0b01101101,
-    0b01111101,
-    0b00000111,
-    0b01111111,
-    0b01101111,
-    0b01110111,
-    0b01111100,
-    0b00111001,
-    0b01011110,
-    0b01111001,
-    0b01110001
-};
 
-PortOut output(PortC, 0xff);
-DigitalOut seg1(PC_12);
-DigitalOut seg2(PC_11);
-bool curr = false;
-
-int num = 0;
-
-
-void timer() {
-    curr = !curr;
-
-    if (curr) {
-        int seg2_num = num & 0b00001111;
-        seg1 = false;
-        seg2 = true;
-        output = segments[seg2_num];
-    } else {
-        int seg1_num = (num & 0b11110000) >> 4;
-        seg2 = false;
-        seg1 = true;
-        output = segments[seg1_num];
-    }
-}
+DigitalOut led(LED1);
 
 int main() {
-    Ticker ticker;
-    ticker.attach(timer, 10ms);
 
     while (true) {
         ThisThread::sleep_for(200ms);
-        num++;
+        led = !led;
     }
 }
